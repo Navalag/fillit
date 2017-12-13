46ee0bfd62c5117a16cb569628d7e9e498337bf7
Maksym MATIUSHCHENKO <mmatiush@e1r6p8.unit.ua>
2017-12-13 14:39:55 +0200

    added figure figure mover by 1 step

--- newway.c
+++ newway.c
@@ -14,16 +14,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+# define TETNUM 4
 
-int edge = 4;
+int g_edge = 6;
 
-typedef struct		tet_t
+typedef struct		s_tet
 {
 	int				x[4];
 	int				y[4];
 	struct tet_t	*next;
 	struct tet_t	*prev;
 }					t_fig;
+
 
 char			**freeing(char **str)
 {
@@ -33,6 +35,7 @@ char			**freeing(char **str)
 	while (str[i] != NULL)
 	{
 		free(str[i++] = NULL);
+		
 	}
 	free(str);
 	return (NULL);
@@ -44,8 +47,8 @@ void to_left(t_fig *fig)
 	int i;
 	
 	i = 0;
-	lowest = edge - 1;
-	while (i < edge)
+	lowest = g_edge - 1;
+	while (i < TETNUM)
 	{
 		if (fig->x[i] < lowest)
 			lowest = fig->x[i];
@@ -55,7 +58,7 @@ void to_left(t_fig *fig)
 	if (lowest == 0)
 		return ;
 	else
-		while (i < edge)
+		while (i < TETNUM)
 		{
 			fig->x[i] -= lowest;
 			i++; 
@@ -68,8 +71,8 @@ void to_up(t_fig *fig)
 	int i;
 	
 	i = 0;
-	lowest = edge - 1;
-	while (i < edge)
+	lowest = g_edge - 1;
+	while (i < TETNUM)
 	{
 		if (fig->y[i] < lowest)
 			lowest = fig->y[i];
@@ -79,7 +82,7 @@ void to_up(t_fig *fig)
 	if (lowest == 0)
 		return ;
 	else
-		while (i < edge)
+		while (i < TETNUM)
 		{
 			fig->y[i] -= lowest;
 			i++; 
@@ -92,7 +95,7 @@ void mv_fig_full_left_up(t_fig *fig)
 	int i;
 	
 	i = 0;
-	while (i < edge)
+	while (i < g_edge)
 	{
 		if (fig->x[i++] == 0)
 			break ;
@@ -103,7 +106,7 @@ void mv_fig_full_left_up(t_fig *fig)
 		}
 	}
 	i = 0;
-	while (i < edge)
+	while (i < g_edge)
 	{
 		if (fig->y[i++] == 0)
 			break ;
@@ -122,10 +125,10 @@ void put_dots_to_field(char **str)
 
 	c_x = 0;
 	c_y = 0;
-	while (c_y < edge)
+	while (c_y < g_edge)
 	{
-		str[c_y] = (char*)malloc(sizeof(char) * (edge + 1));
-		while (c_x < edge)
+		str[c_y] = (char*)malloc(sizeof(char) * (g_edge + 1));
+		while (c_x < g_edge)
 		{
 			str[c_y][c_x] = '.';
 			c_x++;
@@ -139,43 +142,135 @@ void put_dots_to_field(char **str)
 
 void	put_sign_to_field(char **str, t_fig *fig, unsigned char c)
 {
-	int	c_x;
-	int	c_y;
+	int i;
 
-	c_x = 0;
-	c_y = 0;
-	str[c_y][c_x] = 
+	i = 0;
+	while (i < 4)
+	{
+		str[fig->y[i]][fig->x[i]] = c;
+		i++;
+	}
 }
 
-void print_matrix(t_fig *fig)
+void 	print_matrix(t_fig *fig)
+{
+	int 	i;
+	char	**str;
+
+	i = 0;
+	str = (char**)malloc(sizeof(char*) * (g_edge + 1));
+	put_dots_to_field(str);
+	put_sign_to_field(str, fig, 'A');
+	while (i < g_edge)
+		printf("%s\n", str[i++]);
+	freeing(str);
+}
+
+/********************************************************************************************/
+
+void	mv_fig_right(t_fig *fig)
 {
 	int i;
 
 	i = 0;
-	char	**str;
-	str = (char**)malloc(sizeof(char*) * (edge + 1));
-	put_dots_to_field(str);
-	put_sign_to_field(str, fig, '#');
-	while (i < edge)
-		printf("%s\n", str[i++]);
-	freeing(str);
+	while (i < TETNUM)
+	{
+		fig->x[i] += 1;
+		i++;
+	}
 }
+
+void	mv_fig_down(t_fig *fig)
+{
+	int i;
+
+	i = 0;
+	while (i < TETNUM)
+	{
+		fig->y[i] += 1;
+		i++;
+	}
+}
+
+int		mv_fig_once(t_fig *fig)
+{
+	int i;
+	int x_max;
+	int y_max;
+
+	i = 0;
+	x_max = 0;
+	y_max = 0;
+	while (i < TETNUM)
+	{
+		if (fig->x[i] > x_max)
+			x_max = fig->x[i];
+		i++;
+	}
+	i = 0;
+	if (x_max + 1 < g_edge)
+		mv_fig_right(fig);
+	else
+	{
+		while (i < TETNUM)
+		{
+			if (fig->y[i] > y_max)
+				y_max = fig->y[i];
+			i++;
+		}
+		if (x_max + 1 == g_edge && y_max + 1 == g_edge)
+			return (-1);
+		to_left(fig);
+		if (y_max + 1 < g_edge)
+			mv_fig_down(fig);
+	}
+	return 1;
+}
+
+/*void	backtrack(t_fig *head)
+{
+	int		i;
+	int 	j;
+	t_fig	*fig;
+
+	fig = head;
+	i = 0;
+	j = 0;
+	while (i < TETNUM)
+	{
+		while(j < TETNUM)
+		{
+			if (fig->x[i] != fig->next->x[j] && fig->y[i] != fig->next->y[j])
+				j++;
+			else
+			{
+				mv_fig_once(fig->next);
+			}
+		} 
+		i++;
+	}	
+}*/
+
 
 int main()
 {
 	t_fig* ptr;
 	ptr = (t_fig*)malloc(sizeof(t_fig));
-	ptr->x[0] = 1;
-	ptr->x[1] = 0;
-	ptr->x[2] = 1;
-	ptr->x[3] = 1;
-	ptr->y[0] = 1;
-	ptr->y[1] = 2;
-	ptr->y[2] = 2;
-	ptr->y[3] = 3;
-
-	mv_fig_full_left_up(ptr);
+	ptr->x[0] = 4;
+	ptr->x[1] = 3;
+	ptr->x[2] = 4;
+	ptr->x[3] = 4;
+	ptr->y[0] = 0;
+	ptr->y[1] = 1;
+	ptr->y[2] = 1;
+	ptr->y[3] = 2;
 	print_matrix(ptr);
+	printf("\n\n");
+	mv_fig_once(ptr);
+	print_matrix(ptr);
+	printf("\n\n");
+	mv_fig_once(ptr);
+	print_matrix(ptr);
+
 	return 0;
 }
-str[i][j]
